2e83eb45c85acb62e282d4205d612e41076ce26c
Ugo Muhieddine <ugo.muhieddine@aqemia.com>
2026-03-26 10:31:58 +0100

    fix: binding keys

--- custom/smana.zsh
+++ custom/smana.zsh
@@ -11,8 +11,19 @@ setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_FIND_NO_DUPS
 
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+[[ -n "${terminfo[kcuu1]:-}" ]] && bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+[[ -n "${terminfo[kcud1]:-}" ]] && bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+bindkey -M viins "^[[A" up-line-or-beginning-search
+bindkey -M viins "^[[B" down-line-or-beginning-search
+[[ -n "${terminfo[kcuu1]:-}" ]] && bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
+[[ -n "${terminfo[kcud1]:-}" ]] && bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
 
 export TIMEFMT=$'\nreal %E\tuser %U\tsys %S\tpcpu %P'
 export EDITOR=vim
